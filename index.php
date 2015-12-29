5b83b99a94483759f08a4045d553e88c5900bd2b
Peth404 <pethiaschiseke4@gmail.com>
2015-12-29 15:16:16 +0200

    added top ten

--- index.php
+++ index.php
@@ -1,19 +1,288 @@
-<?php
-//include('../../Glimpse/index.php');
-require_once("phpGrid/conf.php");      
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>chess_zambia</title>
-	</head>
-	<body>
-		<?php
-			$dg = new C_DataGrid("SELECT fideid,name,title,rating,rapid_rating,blitz_rating,birthday FROM ratings_list", "fideid", "ratings_list");
-			$dg->enable_advanced_search(true);
-			$dg->enable_export('CSV');
-			$dg -> display();
-		?>
-	</body>
-</html
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Home | Project Chess Zambia</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/price-range.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>
+	<header id="header"><!--header-->
+		<div class="header_top"><!--header_top-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="contactinfo">
+							<ul class="nav nav-pills">
+								<li><a href="#"><i class="fa fa-phone"></i> +260 955 188 821</a></li>
+								<li><a href="#"><i class="fa fa-envelope"></i> info@pcz.com</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header_top-->
+		
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="logo pull-left">
+							<a href="index.html"><img src="images/home/clogo1.png" alt="PCZ logo" /></a>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<h1>Project Chess Zambia</h1>
+					</div>
+					<div class="col-sm-2">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-middle-->
+	
+		<div class="header-bottom"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-9">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="index.html" class="active">News</a></li>
+								<li class="dropdown"><a href="#">Database<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="shop.html">Ratings</a></li>
+										<li><a href="product-details.html">Games</a></li> 
+                                    </ul>
+                                </li> 
+								<li class="dropdown"><a href="#">Tournaments<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="blog.html">Upcoming</a></li>
+										<li><a href="blog-single.html">Completed</a></li>
+                                    </ul>
+                                </li> 
+								<li><a href="404.html">Shop</a></li>
+								<li><a href="contact-us.html">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div class="search_box pull-right">
+							<input type="text" placeholder="Search"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-bottom-->
+	</header><!--/header-->
+	
+	<section id="slider"><!--slider-->
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div id="slider-carousel" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
+							<li data-target="#slider-carousel" data-slide-to="1"></li>
+							<li data-target="#slider-carousel" data-slide-to="2"></li>
+						</ol>
+						
+						<div class="carousel-inner">
+							<div class="item active">
+								<div class="col-sm-4">
+									<!-- <h1><span>E</span>-SHOPPER</h1> -->
+									<!-- <h2>Free E-Commerce Template</h2> -->
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<button type="button" class="btn btn-default get">Buy now</button>
+								</div>
+								<div class="col-sm-8">
+									<img src="images/home/clock1.jpg" class="girl img-responsive" alt="" />
+									<img src="images/home/pricing.png"  class="pricing" alt="" />
+								</div>
+							</div>
+							<div class="item">
+								<div class="col-sm-6">
+									<!-- <h1><span>E</span>-SHOPPER</h1>
+									<h2>100% Responsive Design</h2> -->
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+									consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+									cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+									proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+									<button type="button" class="btn btn-default get">Read More</button>
+								</div>
+								<div class="col-sm-6">
+									<img src="images/home/etcc.jpg" class="girl img-responsive" alt="" />
+								</div>
+							</div>
+							
+							<div class="item">
+								<div class="col-sm-4">
+									<!-- <h1><span>E</span>-SHOPPER</h1>
+									<h2>Free Ecommerce Template</h2> -->
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<button type="button" class="btn btn-default get">Read More</button>
+								</div>
+								<div class="col-sm-8">
+									<img src="images/home/lpca.jpg" class="girl img-responsive" alt="" />
+								</div>
+							</div>
+							
+						</div>
+						
+						<a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
+							<i class="fa fa-angle-left"></i>
+						</a>
+						<a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
+							<i class="fa fa-angle-right"></i>
+						</a>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</section><!--/slider-->
+	
+	<section>
+		<div class="container">
+			<div class="row row-cenetered">
+				<div class="col-centered col-sm-4">
+					<div class="left-sidebar">
+						<h2>Top Ten</h2>
+						<!--category-productsr-->
+						<div class="panel-group category-products" id="accordian">
+						
+							<?php 
+						     $link=mysql_connect("localhost","root"," ") or die("Cannot Connect to the database!");
+								  mysql_select_db("chess_zambia",$link) or die ("Cannot select the database!");
+						   	$query="SELECT * FROM ratings_list order by rating desc limit 10";
+						    
+						      $resource=mysql_query($query,$link);
+						      echo "
+						    <table align=\"center\" border=\"1\" width=\"100%\">
+						    <tr>
+						   <td><b>Name</b></td> <td><b>Rating</b></td> <td><b>Rapid</b></td> <td><b>Blitz</b></td> ";
+							while($result=mysql_fetch_array($resource))
+						  		{ 
+						  		echo "<tr><td>".$result[1]."</td><td>".$result[7]."</td> <td>".$result[10]."</td> <td>".$result[13]."</td>" ;
+						 		 } echo "</table>";
+						  	 ?>
+						</div>
+					</div>
+				</div>
+				
+				<div class="col-centered col-sm-8 padding-right">
+					<div class="features_items"><!--features_items-->
+						<h2 class="title text-center">Latest</h2>
+						<div class="col-centered col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+										<div class="productinfo text-center">
+											<h2>Game of the day</h2>
+											<img src="images/home/cb.jpg" alt="" />
+											<p>Anderssen - Kiezeritzky</p>
+<!-- 											<p>Easy Polo Black Edition</p>
+											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+ -->									</div>
+										<div class="product-overlay">
+											<div class="overlay-content">
+												<!-- <h2>$56</h2>
+												<p>Easy Polo Black Edition</p> -->
+												<a href="#" class="btn btn-default add-to-cart">View Game</a>
+											</div>
+										</div>
+								</div>
+								<div class="choose">
+									<ul class="nav nav-pills nav-justified">
+										<li><a href="#">View similar games</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-centered col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<h2>Puzzle of the day</h2>
+										<img src="images/home/puzzle21.jpg" alt="" />
+										<p>White to play</p>
+									</div>
+									<div class="product-overlay">
+										<div class="overlay-content">
+											<a href="#" class="btn btn-default add-to-cart">View Solution</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<footer id="footer"><!--Footer-->
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="pull-left">Copyright © 2015 PCZ. All rights reserved.</p>
+				</div>
+			</div>
+		</div>
+		
+	</footer><!--/Footer-->
+	
+
+  
+    <script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.scrollUp.min.js"></script>
+	<script src="js/price-range.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
--- phpGrid/conf.php
+++ phpGrid/conf.php
@@ -3,7 +3,7 @@
 
 define('PHPGRID_DB_HOSTNAME', 'localhost'); // database host name
 define('PHPGRID_DB_USERNAME', 'root');     // database user name
-define('PHPGRID_DB_PASSWORD', 'root'); // database password
+define('PHPGRID_DB_PASSWORD', ' '); // database password
 define('PHPGRID_DB_NAME', 'chess_zambia'); // database name
 define('PHPGRID_DB_TYPE', 'mysql');  // database type
 define('PHPGRID_DB_CHARSET','utf8'); // ex: utf8(for mysql),AL32UTF8 (for oracle), leave blank to use the default charset
